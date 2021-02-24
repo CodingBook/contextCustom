55696c4948d228e38221901a1f2e41b66c1eaf8d
Esizsu <esra_izgi@hotmail.com>
2021-02-23 23:38:18 -0800

    not modifed runner

--- src/test/java/org/cb/contextCustom/runner/Runner.java
+++ src/test/java/org/cb/contextCustom/runner/Runner.java
@@ -8,9 +8,16 @@ import org.junit.runner.RunWith;
 @CucumberOptions(glue ={"org.cb.contextCustom.stepdef"},
         features = "src/test/resources/features/",
         // Glue path must start after java folder. (after this -> src/test/java)
+<<<<<<< HEAD
 //        dryRun = false,
 //        strict = false,
 //        tags = "@Home&Living",
+=======
+        glue = "org/cb/contextCustom/stepdef",
+        dryRun =false,
+        strict = false,
+        tags = "@Home&Living",
+>>>>>>> 568ad2b (runner)
         plugin = {"pretty",
                 "html:target/default-cucumber-reports",
                 "json:target/cucumber.json"})
