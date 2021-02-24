52e61802186e67323c33d969488dc68721002a84
Esizsu <esra_izgi@hotmail.com>
2021-02-23 23:43:36 -0800

    modified BrandLogo.java

--- src/test/java/org/cb/contextCustom/runner/Runner.java
+++ src/test/java/org/cb/contextCustom/runner/Runner.java
@@ -4,23 +4,21 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+
 @RunWith(Cucumber.class)
-@CucumberOptions(glue ={"org.cb.contextCustom.stepdef"},
+@CucumberOptions(
         features = "src/test/resources/features/",
         // Glue path must start after java folder. (after this -> src/test/java)
-<<<<<<< HEAD
-//        dryRun = false,
-//        strict = false,
-//        tags = "@Home&Living",
-=======
         glue = "org/cb/contextCustom/stepdef",
         dryRun =false,
         strict = false,
-        tags = "@Home&Living",
->>>>>>> 568ad2b (runner)
+        tags = "@Kids",
         plugin = {"pretty",
                 "html:target/default-cucumber-reports",
-                "json:target/cucumber.json"})
+                "json:target/cucumber.json"
+        }
+)
 public class Runner {
-
 }
+
+
