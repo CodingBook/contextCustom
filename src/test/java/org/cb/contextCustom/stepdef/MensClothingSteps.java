f73eb8a86ea732beb909e4676f6787f2f859ee1b
mehmetvural2020 <mehmetvurall@gmail.com>
2021-02-25 13:54:05 +0700

    I updated MensClothing.feature and their steps classes.

--- src/test/java/org/cb/contextCustom/stepdef/MensClothingSteps.java
+++ src/test/java/org/cb/contextCustom/stepdef/MensClothingSteps.java
@@ -15,6 +15,7 @@ public class MensClothingSteps extends Base{
 
     @And("move and click on All Men's Clothing")
     public void moveAndClickOnAllMenSClothing() {
+
         moveAndClickToElement(mensClothing.allMen);
     }
 
@@ -23,5 +24,40 @@ public class MensClothingSteps extends Base{
         String expected = "Men's Clothing";
         waitSomeTime(1000L);
         textIsDisplayedAndEnabled(expected, mensClothing.mensClothingText);
+    }
+
+    @And("verify that first men item image is present")
+    public void verifyThatFirstMenItemImageIsPresent() {
+        imageIsDisplayedAndEnabled(mensClothing.firstItemImage);
+    }
+
+    @And("verify that men item brand name is present")
+    public void verifyThatMenItemBrandNameIsPresent() {
+        String expected = "Bella canvas";
+        textIsDisplayedAndEnabled(expected, mensClothing.firstItemBrandName);
+    }
+
+    @Then("verify that men item title is present")
+    public void verifyThatMenItemTitleIsPresent() {
+        String expected = "Unisex CVC SS Tee | 3001 Bella Canvas";
+        textIsDisplayedAndEnabled(expected, mensClothing.firstItemTitle);
+    }
+
+    @And("verify that men item price is present")
+    public void verifyThatMenItemPriceIsPresent() {
+        String expected = "$9.99";
+        textIsDisplayedAndEnabled(expected, mensClothing.firstItemPrice);
+    }
+
+    @And("click on men kids item picture")
+    public void clickOnMenKidsItemPicture() {
+        click(mensClothing.firstItemImage);
+        waitSomeTime(1000L);
+    }
+
+    @Then("verify that first men item name is present")
+    public void verifyThatFirstMenItemNameIsPresent() {
+        String expected = "Unisex CVC SS Tee …";
+        textIsDisplayedAndEnabled(expected, mensClothing.firstItemName);
     }
 }
