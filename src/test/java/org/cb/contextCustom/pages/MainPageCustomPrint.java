93744168ec469b422a19ffa45e82011b14bdcb7d
ZennT <zenn.diamond@gmail.com>
2021-02-26 10:09:45 -0800

    updated MainPageCustomPrint.java

--- src/test/java/org/cb/contextCustom/pages/MainPageCustomPrint.java
+++ src/test/java/org/cb/contextCustom/pages/MainPageCustomPrint.java
@@ -7,13 +7,6 @@ import org.openqa.selenium.support.PageFactory;
 
 public class MainPageCustomPrint {
 
-    import org.cb.contextCustom.utils.MyDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-
-    public class MainPageCustomPrint {
-
         public MainPageCustomPrint(){
             PageFactory.initElements(MyDriver.get(),this);
         }
